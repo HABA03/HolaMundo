b5085cd3c92e7150d1da1f874ab437d48c135b1f
damaris Baldenegro <damarisbaldenegro@gmail.com>
2014-09-24 10:51:39 -0500

    Base del proyecto

---
+++ holamundo1/holamundo1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Damaris Baldenegro
+ * Date: 24/09/2014
+ * Time: 10:46 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace holamundo1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
